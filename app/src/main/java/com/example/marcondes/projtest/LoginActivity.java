38ee089e4229eb269d6a455b99cd495819535061
Marcondes Amorim <marcondesjra@gmail.com>
2018-03-05 12:10:23 -0300

    First commit

--- app/src/main/java/com/example/marcondes/projtest/LoginActivity.java
+++ app/src/main/java/com/example/marcondes/projtest/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.marcondes.projtest;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -23,10 +26,21 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginActivity(View v) {
+        EditText emailText;
+        EditText passwordText;
         Intent it = new Intent(LoginActivity.this, MainActivity.class);
+        emailText = (EditText)findViewById(R.id.email);
+        passwordText = (EditText) findViewById(R.id.password);
+        if(users.contains(new User(emailText.getText().toString(), passwordText.getText().toString()))){
+            startActivity(it);
+        } else{
+           Context context = getApplicationContext();
+            CharSequence text = "Usuaário/email inválido !";
+            int duration = Toast.LENGTH_SHORT;
 
-
-        startActivity(it);
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
     public class User{
